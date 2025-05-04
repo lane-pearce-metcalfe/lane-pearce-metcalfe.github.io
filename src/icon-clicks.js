0304cfb0d5785708cf847cda9b40f3c99156233c
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-05-04 14:01:06 +1200

    Minesweeper icon on main page now brings you to the minesweeper game

--- src/icon-clicks.js
+++ src/icon-clicks.js
@@ -1,9 +1,13 @@
 export function iconClicks() {
   document.getElementById('internet').addEventListener('click', () => {
     window.location.href = `https://www.google.com/`
-  });
+  })
 
   document.getElementById('blog-folder').addEventListener('click', () => {
-    document.getElementById('main-folder').style.display = 'flex';
+    document.getElementById('main-folder').style.display = 'flex'
+  })
+
+  document.getElementById('minesweeper').addEventListener('click', () => {
+    window.location.href = `minesweeper.html`
   })
 }
